48a4bd1dec322746222bd3d7ecb6297d8ed6b7e1
Dhiva Kesuma Pertiwi <dhivakesuma@gmail.com>
2025-10-08 16:14:12 +0800

    Part 2 – Using Named Routes

--- lab_activity/lib/main.dart
+++ lab_activity/lib/main.dart
@@ -7,7 +7,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Navigation Lab',
-      home: FirstScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/details': (context) => DetailsScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/details');
+          },
+          child: Text('Open Details'),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Details Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Back to Home'),
+        ),
+      ),
     );
   }
 }
