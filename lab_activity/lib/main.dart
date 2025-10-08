6a8464789816b55d8c941ee999a55680a9be767f
Dhiva Kesuma Pertiwi <dhivakesuma@gmail.com>
2025-10-08 17:19:39 +0800

    step 3 - 6

--- lab_activity/lib/main.dart
+++ lab_activity/lib/main.dart
@@ -1,39 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:app_links/app_links.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final AppLinks _appLinks;
+  String _status = 'Waiting for link...';
+
+  @override
+  void initState() {
+    super.initState();
+    initDeepLinks();
+  }
+
+  Future<void> initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // Handle link ketika app pertama kali dibuka
+    final Uri? initialLink = await _appLinks.getInitialLink();
+    if (initialLink != null) _handleLink(initialLink);
+
+    // Dengarkan link baru saat app masih hidup
+    _appLinks.uriLinkStream.listen((Uri uri) {
+      _handleLink(uri);
+    });
+  }
+
+  void _handleLink(Uri uri) {
+    setState(() => _status = 'Received link: $uri');
+
+    if (uri.host == 'details') {
+      final id = uri.pathSegments.isNotEmpty ? uri.pathSegments[0] : 'unknown';
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => DetailScreen(id: id)),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Deep Link Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/details': (context) => DetailScreen(),
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Text('Welcome! Try opening myapp://details/42'),
+      title: 'App Links Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: Center(
+          child: Text(_status),
+        ),
       ),
     );
   }
 }
 
 class DetailScreen extends StatelessWidget {
+  final String id;
+  const DetailScreen({required this.id});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Details')),
-      body: Center(child: Text('You are on the Detail screen!')),
+      appBar: AppBar(title: const Text('Details')),
+      body: Center(child: Text('You opened item ID: $id')),
     );
   }
 }
