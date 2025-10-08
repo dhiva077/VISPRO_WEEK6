026fb7ef450d6c4b04dcea505fbb3b5c9876edce
Dhiva Kesuma Pertiwi <dhivakesuma@gmail.com>
2025-10-08 15:58:58 +0800

    step 1-3 part 1

---
+++ lab_activity/android/app/src/main/kotlin/com/example/lab_activity/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab_activity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- session5_assignments/android/app/src/main/kotlin/com/example/session5_assignments/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.session5_assignments
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
